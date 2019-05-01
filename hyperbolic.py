7eaf915fcc5876e92c8aa8c7bd03273108db58e9
Prakash Murali <prakashmurali@gmail.com>
2019-04-30 21:46:29 -0400

    added simple hyperbolic cache

---
+++ hyperbolic.py
@@ -0,0 +1,64 @@
+import random
+
+#Priority function = #accesses/lifetime in cache
+
+class HyperbolicCache:
+    def __init__(self, capacity, sample_size):
+        self.capacity = capacity
+        self.store_dict = {}
+        self.key_costs = {}
+        self.key_access_cnt = {}
+        self.key_lifetime = {}
+        self.sample_size = sample_size
+        
+    def put(self, key, value, cost):
+        if len(self.store_dict.keys()) >= self.capacity:
+            evict_key = self.find_key_to_evict()
+            del self.store_dict[evict_key]
+            del self.key_costs[evict_key]
+            del self.key_access_cnt[evict_key]
+            del self.key_lifetime[evict_key]
+            print("Evicted", evict_key)
+        self.store_dict[key] = value
+        self.key_costs[key] = cost
+        self.key_access_cnt[key] = 1
+        self.key_lifetime[key] = 0
+        #Increment all lifetime counters by 1 for this timestep
+        for k in self.key_lifetime.keys():
+            self.key_lifetime[k] += 1
+        self.print_cache()
+    def get(self, key):
+        value = None
+        if key in self.store_dict.keys():
+            value = self.store_dict[key]
+        else:
+            print("Cache miss", key)
+        #Increment all lifetime counters by 1 for this timestep
+        for k in self.key_lifetime.keys():
+            self.key_lifetime[k] += 1
+        self.print_cache()
+        return value
+
+    def find_key_to_evict(self):
+        sample = random.sample(self.store_dict.keys(), min(len(self.store_dict.keys()), self.sample_size))
+        priority_dict = {}
+        for k in sample:
+            priority_dict[k] = float(self.key_access_cnt[k])/self.key_lifetime[k]
+        return min(priority_dict, key=priority_dict.get)
+        
+
+    def print_cache(self):
+        #print(self.store_dict)
+        priority_dict = {}
+        for k in self.store_dict:
+            priority_dict[k] = float(self.key_access_cnt[k])/self.key_lifetime[k]
+        print("Priority:", priority_dict) 
+def hyper_test():
+    cache = HyperbolicCache(5, 3)
+    for i in range(100):
+        cache.put(i, 0, 0)
+        cache.put(1000, 0, 0)
+
+    cache.print_cache()
+
+hyper_test()
---
+++ lru_cache.py
@@ -0,0 +1,80 @@
+
+class BackendCache:
+    def __init__(self, capacity, policy):
+        self.capacity = capacity
+        self.lru_queue = []
+        self.store_dict = {}
+        self.policy = policy
+        self.key_costs = {}
+        self.walk_length = 10
+    
+    def put(self, key, value, cost):
+        if len(self.store_dict.keys()) >= self.capacity:
+            evict_key = self.find_key_to_evict()
+            self.lru_queue.remove(evict_key)
+            del self.store_dict[evict_key]
+            print("Evicted", evict_key)
+        self.store_dict[key] = value
+        self.lru_queue.append(key)
+        self.key_costs[key] = cost
+
+    def get(self, key):
+        if key in self.store_dict.keys():
+            value = self.store_dict[key]
+            self.lru_queue.remove(key)
+            self.lru_queue.append(key)
+        else:
+            print("Cache miss", key)
+
+    def find_key_to_evict(self):
+        if self.policy == "LRU":
+            #return head of the queue
+            return self.lru_queue[0]
+        elif self.policy == "NetLRU":
+            i = 0
+            min_cost = self.key_costs[self.lru_queue[0]]
+            min_idx = self.lru_queue[0]
+            #print("Queue:", self.lru_queue)
+            for item in self.lru_queue:
+                if min_cost > self.key_costs[item]:
+                    min_cost = self.key_costs[item]
+                    min_idx = item
+                i += 1
+                if i == self.walk_length:
+                    break
+            #print("returning", min_idx)
+            return min_idx
+
+    def print_cache(self):
+        print(self.store_dict)
+        print(self.lru_queue)
+
+def lru_test():
+    cache = BackendCache(3, "LRU")
+    cache.put(1, 0, 0)
+    cache.put(2, 0, 0)
+    cache.put(3, 0, 0)
+    cache.get(2)
+    cache.put(4, 0, 0)
+    cache.get(1)
+    cache.put(5, 0, 0)
+    cache.put(6, 0, 0)
+    cache.get(5)
+    cache.print_cache()
+
+def netlru_test():
+    print("")
+    cache = BackendCache(3, "NetLRU")
+    cache.put(1, 0, 100)
+    cache.put(2, 0, 0)
+    cache.put(3, 0, 0)
+    cache.get(2)
+    cache.put(4, 0, 0)
+    cache.get(1)
+    cache.put(5, 0, 0)
+    cache.put(6, 0, 0)
+    cache.get(5)
+    cache.print_cache()
+
+lru_test()
+netlru_test()
